bf9550f85ab464f84336854e06f2a9341d0aa92a
nefjffk000-code <nefjffk000@gmail.com>
2026-06-07 06:59:16 +0600

    Create wingo.js

---
+++ wingo.js
@@ -0,0 +1,15 @@
+// api/wingo.js
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://draw.ar-lottery01.com/WinGo/WinGo/GetHistorys', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
